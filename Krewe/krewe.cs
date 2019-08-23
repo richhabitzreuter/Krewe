e2c2a03c23d90f4eeb2eec5c3552d17d01032bbc
yungxhrist <doesnotmatteru@gmail.com>
2019-08-23 02:44:38 -0300

    filosofem

---
+++ Krewe/krewe.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Krewe
+{
+    public class krewe
+    {
+        private static bool lowPriority = false;
+
+        private static string[] appList = { "devenv", "Discord", "chrome", "brave", "LeagueClient" };
+
+        private static void SetLowPriority(bool reset = false)
+        {
+            for (int i = 0; i < appList.Length; i++)
+            {
+                var processes = Process.GetProcessesByName(appList[i]);
+                for (int y = 0; y < processes.Length; y++)
+                {
+                    processes[y].PriorityClass = reset ? ProcessPriorityClass.Normal : ProcessPriorityClass.Idle;
+                }
+            }
+            lowPriority = !reset;
+        }
+
+        private static bool CheckProcesses(string processName)
+        {
+            var processes = Process.GetProcesses();
+            for (int i = 0; i < processes.Length; i++)
+            {
+                if (processes[i].ProcessName == processName)
+                {
+                    processes[i].PriorityClass = ProcessPriorityClass.High;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static void Main(string[] args)
+        {
+            var processName = "League of Legends";
+            var sleepDelay = 3000;
+            var keepRunning = true;
+
+            try
+            { 
+                processName = args[0];
+                sleepDelay = int.Parse(args[1]) * 1000;
+                keepRunning = args[2] == "true" ? true : false;
+            }
+            catch { }
+
+            while (true)
+            {
+                if (!CheckProcesses(processName))
+                {
+                    if (!keepRunning)
+                    {
+                        SetLowPriority(true);
+                        break;
+                    }
+                    else
+                    {
+                        if (lowPriority)
+                        {
+                            SetLowPriority(true);
+                        }
+                    }
+                }
+                else if (keepRunning && !lowPriority)
+                {
+                    SetLowPriority();
+                }
+                GC.Collect();
+                Thread.Sleep(sleepDelay);
+            }
+        }
+    }
+}
